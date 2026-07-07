1d314ecb776671029909e85a4173fe66b0fcd8d1
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-07-07 16:54:57 +0300

    adding new page: homepage

--- Bayn/frontend/src/app/App.jsx
+++ Bayn/frontend/src/app/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import LoginPage  from '@/features/identity/pages/LoginPage';
 import SignUpPage from '@/features/identity/pages/SignUpPage';
- 
+import HomePage   from '@/features/home/pages/HomePage';
+
 export default function App() {
-  const [page, setPage] = useState('login');
+  const [page, setPage] = useState('home');
   return (
     <>
       {page === 'login'  && <LoginPage  onNavigate={setPage} />}
       {page === 'signup' && <SignUpPage onNavigate={setPage} />}
+      {page === 'home'   && <HomePage   onNavigate={setPage} />}
     </>
   );
 }
